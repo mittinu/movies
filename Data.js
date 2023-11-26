73f8b296b0e6d9747585c00b5ac26db7834970b5
SeoulZiggs <145526609+mittinu@users.noreply.github.com>
2023-11-26 12:44:28 +0900

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,49 @@
+let data = [
+    {
+        id : 0,
+        title : "Star Wars",
+        imageUrl : `${process.env.PUBLIC_URL}/Images/starwars.jpeg`,
+    },
+    {
+        id : 1,
+        title : "God Father",
+        imageUrl : `${process.env.PUBLIC_URL}/Images/godfather.jpeg`
+    },
+    {
+        id : 2,
+        title : "Jaws",
+        imageUrl : `${process.env.PUBLIC_URL}/Images/jaws.webp`
+    },
+    {
+        id : 3,
+        title : "Babylon",
+        imageUrl : `${process.env.PUBLIC_URL}/Images/babylon.webp`
+    },
+    {
+        id : 4,
+        title : "Spider Man",
+        imageUrl : `${process.env.PUBLIC_URL}/Images/spiderman.webp`
+    },
+    {
+        id : 5,
+        title : "Rocky",
+        imageUrl : `${process.env.PUBLIC_URL}/Images/rocky.webp`
+    },
+    {
+        id : 6,
+        title : "Jurassic Park",
+        imageUrl : `${process.env.PUBLIC_URL}/Images/jurassicpark.webp`
+    },
+    {
+        id : 7,
+        title : "Avengers",
+        imageUrl : `${process.env.PUBLIC_URL}/Images/avengers.webp`
+    },
+    {
+        id : 8,
+        title : "Home Alone",
+        imageUrl : `${process.env.PUBLIC_URL}/Images/homealone.webp`
+    }
+];
+
+export default data;
